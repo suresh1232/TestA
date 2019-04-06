5a2cd439bf47988c2fb66c4adb2a0ad4627ca041
chikku <chikku@example.com>
2019-04-06 06:57:47 -0700

    Raju

--- src/com/dsss/collection/Idcomp.java
+++ src/com/dsss/collection/Idcomp.java
@@ -7,6 +7,7 @@ public class Idcomp implements Comparator<Product> {
 	
 	@Override
 	public int compare(Product p1, Product p2) {
+System.out.println("done");
 		if(p2.id==p1.id)
 			return 0;
 		else if(p2.id>p1.id) 
@@ -16,7 +17,8 @@ public class Idcomp implements Comparator<Product> {
 		return "Suresh test";
 System.out.println("done");
 
-
+System.out.println("done");
+System.out.println("done");
 
 			
 	}
